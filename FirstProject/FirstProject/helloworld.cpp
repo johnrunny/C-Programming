a5f63ac9fad04028771443b107b6db6df00766a7
johnrunny <wangxuehan0802@163.com>
2018-05-04 17:31:41 +0800

    使用VisualStudio2017 Enterprice集成gitHub实现C语言版本的helloworld

---
+++ FirstProject/FirstProject/helloworld.cpp
@@ -0,0 +1,8 @@
+#include "common.h" //引入公共的头文件
+/*
+	使用VisualStudio开发的第一个C语言程序 需要共享到gitHub上
+*/
+void main() {
+	printf("hello world in windows7 developed by VisualStudio2017 Enterprice");
+	getchar();//等待输入任意字符退出程序
+}
